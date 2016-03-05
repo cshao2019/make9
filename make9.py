ba1d255ed9051aa64b5332c697fac3827ae0b1b1
Christine <Christine@Christines-Air.home>
2016-03-05 11:58:34 -0500

    Fixed Make9 program

--- make9.py
+++ make9.py
@@ -1,32 +1,26 @@
 import random
 score = 0
-response = raw_input("Your score is " + str(score) + ". Do you want more points? ") #answer with yes or no
+#answer with yes or no
+response = raw_input("Your score is " + str(score) + ". Do you want more points? ") 
 
 while response == "yes":
 	points = random.uniform(0.0,3.0)
 	score = score + points
-	response = raw_input("Your score is " + str(score) + ". Do you want more points? ")
+	#need this condition because if score is >9, cannot show this message
+	#essentially first comparing the new score with 9 to determine to show below message or not
+	if score < 9: 
+		response = raw_input("Your score is " + str(score) + ". Do you want more points? ")
+	elif score == 9:
+		print("Your score is " + str(score) + ". You win!")
+		exit() #won't have the infinite loop
+	else:
+		print("Your score is " + str(score) + ". Since it is over 9, you lose!")
+		exit() #won't have the infinite loop
 
-if score == 9:
-	print("Your score is " + str(score) + ". You win!")
-elif score < 9:
-	print("Your score is " + str(score) + ". Not bad, at least you didn\'t go over 9!")
-elif score > 9:
-	print("Your score is " + str(score) + ". You lose!")
-	
-
-#Planning:
-#score = 0
-#loop pick number (random) 
-# Your score is new random number + previous number(score). Do you want more points? answer with yes or no
-#when yes, then repeat the two previous hashtags. - this is in a loop
-
-#when no, then say whether you win if = 9 and you lose when greater than 9 and not bad if it is less than 9
-
-	
-
-
-
-
-
-
+if response != "yes": #no
+	if score == 9:
+		print("Your score is " + str(score) + ". You win!")
+	elif score < 9:
+		print("Your score is " + str(score) + ". Not bad, at least you didn\'t go over 9!")
+	#don't need "if score > 9:" because as soon as score is > 9, the "You lose" message 
+	#will show and the program will end. Will then never get to this part. 
